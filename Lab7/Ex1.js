6d2b5091ab0ad8446e20434a67eb7cbb2ba3de73
rkisaba <rkisaba@hawaii.edu>
2022-02-22 21:11:10 -1000

    LAB7

--- Lab7/Ex1.js
+++ Lab7/Ex1.js
@@ -8,7 +8,15 @@ while(counter <= num_products){
     console.log(`${counter}. ${eval('name' + counter)}` );
     counter++;
     if(counter > num_products/2){
+        console.log("Don't ask for anything else")
+        process.exit();
         break;
     }
 }
 console.log("That's all we have!")
+
+
+for(counter = 1; eval("typeof name"+counter) != 'undefined'; counter++){
+    console.log(`${counter}. ${eval('name' + counter)}` );
+    if(counter > num_products/2){
+        console.log("Don't ask for anything else")
