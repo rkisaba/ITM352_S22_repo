2f033951316a700ec56bdc151e110748b4db6152
rkisaba <rkisaba@hawaii.edu>
2022-04-16 15:38:09 -1000

    final changes

--- Kisaba_Reese_Assignment2/server.js
+++ Kisaba_Reese_Assignment2/server.js
@@ -19,6 +19,8 @@ else {
     user_data = {};
 }
 
+var temp_qty_data = {};
+
 // Importing parser and querystring
 var myParser = require("body-parser");
 const queryString = require('querystring');
@@ -40,27 +42,31 @@ function isNonNegInt(q, returnErrors = false) {
 app.use(express.urlencoded({ extended: true }));
 // Checks for the existence of the file, from Lab 14
 if (fs.existsSync(filename)) {
-    var data = fs.readFileSync(filename, 'utf-8'); // if it exists, read the file user_data.json storedin filename
-    var user_data = JSON.parse(data); // parse user data
+    var file_stats = fs.statSync(filename);
+    console.log(filename + 'has' + file_stats.size + 'characters!');
+    data = fs.readFileSync(filename, 'utf-8'); //read in the data
+    user_data = JSON.parse(data);
+} else {
+    console.log(filename + ' does not exist!');
 }
 
 app.post("/process_login", function (req, res) {
     var LogError = [];
     console.log(req.query);
-    the_username = req.body.username.toLowerCase; //username in lowercase
+    the_username = req.body.username.toLowerCase(); //username in lowercase
     if (typeof user_data[the_username] != 'undefined') { //matching username
         if (user_data[the_username].password == req.body.password) {
             console.log(req.query);
             req.query.username = the_username;
-            console.log(user_data[req.query.username].name);
-            req.query.name = user_data[req.query.username].name
+            console.log(user_data[req.query.username].username);
+            req.query.name = user_data[req.query.username].username
             res.redirect('/invoice.html?' + queryString.stringify(req.query));
             return; // all good, send to invoice
         } else { //password wrong, show invalid password
             LogError.push = ('Invalid Password');
             console.log(LogError);
             req.query.username = the_username;
-            req.query.name = user_data[the_username].name;
+            req.query.name = user_data[the_username].username;
             req.query.LogError = LogError.join(';');
         }
     } else { //push to the user invalid username if username is incorrect 
@@ -73,8 +79,6 @@ app.post("/process_login", function (req, res) {
 });
 
 
-// POST request from signup.html. Received help from Professor Port
-// Registration validation (each validation adopted from https://www.w3resource.com/javascript/form/javascript-sample-registration-form-validation.php)
 app.post("/process_register", function (req, res) {
     var reg_errors = {};  // assume no errors at start
     var reg_email = req.body.email.toLowerCase();
@@ -82,10 +86,10 @@ app.post("/process_register", function (req, res) {
 
     // EMAIL VALIDATION
     if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(req.body.email)) { //check if the Email is correct
-    }
+    } 
     else {
         reg_errors['email'] = "Invalid email."; // error message for email that doesn't match format of name@website.domain
-    }
+    } 
     if (typeof user_data[reg_email] != 'undefined') { // Email must be unique
         reg_errors['email'] = 'This email is already taken!'; // error message if username is already in user_data.json file
     }
@@ -95,7 +99,7 @@ app.post("/process_register", function (req, res) {
     } else {
         reg_errors['fullname'] = "Enter a name with alphabet characters only."; // error message if name contains numbers or invalid characters/symbols
     }
-
+    
     if (req.body.fullname.length > 30 && req.body.fullname.length < 1) { // 30 characters or less
         reg_errors['fullname'] = "Name must be less than 30 characters."; // error message if name exceeds 30 characters or nothing in there
     }
@@ -110,7 +114,7 @@ app.post("/process_register", function (req, res) {
     if (req.body.username.length > 10 || req.body.username.length < 4) { // 4-10 characters
         reg_errors['username'] = "Username must be 4-10 characters."; // error message if username is under 4 characters or over 10 characters
     }
-
+    
     if (typeof user_data[reg_username] != 'undefined') { // Username must be unique
         reg_errors['username'] = 'This username is already taken!'; // error message if username is already in user_data.json file
     }
@@ -125,25 +129,25 @@ app.post("/process_register", function (req, res) {
         reg_errors['confirmpassword'] = "Passwords do not match." // error message if passwords don't match
     }
 
-
+    
     // Save registration data to json file and send to invoice page if registration successful. 
     // Taken from Lab 14 Exercise 4.
-    if (JSON.stringify(reg_errors) == '{}') {
-        var email = req.body['email'].toLowerCase();
+    if (JSON.stringify(reg_errors) == '{}') { 
+        var email= req.body['email'].toLowerCase();
         user_data[email] = {};
         // information entered is added to user_data
-        user_data[email]['name'] = req.body['fullname'];
+        user_data[email]['name'] = req.body['fullname']; 
         user_data[email]['password'] = req.body['password'];
         user_data[email]['email'] = req.body['email'];
 
         // stored data of purchase info goes into temp_qty_data
-        fs.writeFileSync(filename, JSON.stringify(user_data), "utf-8");
+       fs.writeFileSync(filename, JSON.stringify(user_data), "utf-8");
 
         // username and email from temp_qty_data variable added into file as username and email
         temp_qty_data['email'] = email;
         temp_qty_data['email'] = user_data[email]["username"];
         let params = new URLSearchParams(temp_qty_data);
-        res.redirect('./invoice.html?' + params.toString()); return; // go to invoice at the end of a successful registration process
+        res.redirect('./invoice.html?' + params.toString());return; // go to invoice at the end of a successful registration process
     }
 
     // Otherwise back to registration with the registration errors.    
@@ -151,9 +155,8 @@ app.post("/process_register", function (req, res) {
         req.body['reg_errors'] = JSON.stringify(reg_errors);
         let params = new URLSearchParams(req.body);
         res.redirect('register.html?' + params.toString()); // redirect to signup page after errors popup
-    }
+      }
 });
-
 // Inputted quantities are less than stock
 function validatestock_quantity(quantity_input, stock_quantity) {
     if (quantity_input > stock_quantity) {
